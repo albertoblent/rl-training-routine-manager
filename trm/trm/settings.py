7cd9e7ab2916cc1b339ca6899efec5b373797b3f
Barnett L <blobel@advancelocal.com>
2024-09-03 19:00:53 -0400

    update settings.py

--- trm/trm/settings.py
+++ trm/trm/settings.py
@@ -26,6 +26,10 @@ if not DEBUG:
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
